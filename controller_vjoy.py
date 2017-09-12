3662c450d11ae1ed812456d0e39584765ab19215
asmith9455 <asmith9455>
2017-09-12 14:44:00 -0400

    tsb control through vjoy complete

--- controller_vjoy.py
+++ controller_vjoy.py
@@ -1,34 +1,89 @@
+import sys,site
 pyjoy_path = sys.path[0] + '\\pyvjoy'
 site.addsitedir(pyjoy_path)       # from https://github.com/tidzo/pyvjoy
 import pyvjoy
+import time
+
 
 MAX_VJOY = 32767    #max value for analog inputs (min value is 1 (or 0?))
 
-class controller_sim:
+class controller_vjoy:
+
+    last_t = 0.0
+    last_s = 0.0
+    last_b = 0.0
+
     def __init__ (self, devId):
-        self.vj = pyvjoy.VJoyDevice(devId)
+        self.vj = pyvjoy.VJoyDevice(1)
+        self.set_tsb(0.0,0.0,0.0)
 
     def set_t(self, t):
         #throttle
         self.vj.data.wAxisXRot = int(t * MAX_VJOY)
+        self.vj.update()
+        if t != self.last_t:
+            print("throttle changed from ", self.last_t, " to ", t)
+            self.last_t = t
 
     def set_s(self, s):     #-1.0 for full left, +1.0 for full right
         tmp_s = (s + 1.0) / 2.0
         self.vj.data.wAxisX = int(tmp_s * MAX_VJOY)
+        self.vj.update()
+        if s != self.last_s:
+            print("steering changed from ", self.last_s, " to ", s)
+            self.last_s = s
 
     def set_b(self, b):
         self.vj.data.wAxisYRot = int(b * MAX_VJOY)
+        self.vj.update()
+        if b != self.last_b:
+            print("braking changed from ", self.last_b, " to ", b)
+            self.last_b = b
 
     def reset(self):
         self.vj.reset()
         self.set_tsb(0.0, 0.0, 0.0)
+        self.vj.update()
 
     def set_tsb(self, t, s, b):
         self.set_t(t)       #throttle
         self.set_s(s)       #steering
         self.set_b(b)       #braking
 
+    def delay(self, delayTime, countdownText):
+       for i in range(delayTime)[::-1]:
+            print(countdownText,": ", i+1)
+            time.sleep(1)
 
+    def pulse_btn(self, btnNum, delay = 3, hold = 1):
+        self.vj.set_button(btnNum, 0)
+        self.delay(delay, "delay")
+        self.vj.set_button(btnNum, 1)
+        self.delay(hold, "hold")
+        self.vj.set_button(btnNum, 0)
 
+    def pulse(self, tsb_char, start_and_end_val, pulse_val, delay = 3, hold = 1):
+
+        set_fcn = None
+
+        if tsb_char == 't':
+            set_fcn = self.set_t
+            print("pulsing throttle")
+        elif tsb_char == 's':
+            set_fcn = self.set_s
+            print("pulsing steering")
+        elif tsb_char == 'b':
+            set_fcn = self.set_b
+            print("pulsing braking")
+
+        set_fcn(start_and_end_val)
+
+        self.delay(delay, "delay")
         
+        set_fcn(pulse_val)
+        print("pulsing signal and holding")
+        self.delay(hold, "hold")
         
+
+        set_fcn(start_and_end_val)
+        print("releasing value")
--- main.py
+++ main.py
@@ -1,77 +1,17 @@
 from key_io import *
-import sys,site
-
-import time
-
-MAX_VJOY = 32767
-j = pyvjoy.VJoyDevice(1)
-
-def play_function(X,Y,Z,XRot):
-    j.data.wAxisX = int(X * MAX_VJOY)
-    j.data.wAxisY = int(Y * MAX_VJOY)
-    j.data.wAxisZ = int(Z * MAX_VJOY)
-    j.data.wAxisXRot = int(XRot * MAX_VJOY)
-    
-    j.update()
-
-def set_tsb(t, s, b):
-
-    #steering
-    tmp_s = (s + 1.0) / 2.0
-    j.data.wAxisX = int(tmp_s * MAX_VJOY)
-    
-    #throttle
-    j.data.wAxisXRot = int(t * MAX_VJOY)
-    
-    #braking
-    j.data.wAxisYRot = int(b * MAX_VJOY)
-
-
-
-
-def x_ax_left():
-    #Set X axis to fully left
-    j.set_axis(pyvjoy.HID_USAGE_X, 0x1)
-
-def x_ax_middle():
-    #Set X axis to fully right
-    j.set_axis(pyvjoy.HID_USAGE_X, 0x4000)
-
-def x_ax_right():
-    #Set X axis to fully right
-    j.set_axis(pyvjoy.HID_USAGE_X, 0x8000)
-
-def set_btn_15():
-    j.set_button(5,1)
-
-def reset_btn_15():
-    j.set_button(5,0)
-
-
-def pulse_left():
-    
+from controller_vjoy import controller_vjoy
 
 
 if __name__ == "__main__":
-    print("resetting controller")
-    #play_function(0.5, 0.5, 0, 0)
-    x_ax_middle()
-    #goLeft()
-    print("3")
-    time.sleep(1)
-    print("2")
-    time.sleep(1)
-    print("1")
-    time.sleep(1)
+    ctrler = controller_vjoy(1)
+    #ctrler.pulse_btn(5, delay = 5, hold = 1)
     
-    #steer left, steer right, throttle, brake
+    ctrler.pulse('b', 0.0, 1.0, delay = 5, hold = 1)
 
-    #play_function(0, 0.5, 0, 0)
-    x_ax_right()
-    print("set value, holding for 1 second")
-    time.sleep(2)
-
-
-    #play_function(0.5, 0.5, 0, 0)
-    x_ax_middle()
-    print("releasing value")
+    ctrler.pulse('s', 0.0, 0.25, delay = 0, hold = 1)
+    ctrler.pulse('s', 0.0, -0.25, delay = 0, hold = 1)
+    
+    ctrler.pulse('b', 0.0, 1.0, delay = 5, hold = 10)
+    #ctrler.pulse('t', 0.0, 1.0, delay = 0, hold = 5)
+    #ctrler.pulse('b', 0.0, 1.0, delay = 0, hold = 10)
+    
